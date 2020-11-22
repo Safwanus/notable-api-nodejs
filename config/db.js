e286cfdf1c707ebf6584831d42662f30fcf245d9
Safouane El-amine BENELKADI <49530263+Safwanus@users.noreply.github.com>
2020-11-22 02:36:26 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : "mongodb+srv://user:pass123@notable-nodes.oovzr.mongodb.net/notable?retryWrites=true&w=majority"
+    url : "mongodb+srv://user:<PASSWORD>@notable-nodes.oovzr.mongodb.net/notable?retryWrites=true&w=majority"
 }
