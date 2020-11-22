817ff8edf3fac4d756d1236fe293045e7ffce8e5
Safwanus <s.benelkadi@outlook.fr>
2020-11-22 02:17:28 +0100

    Post / Get functionnal

--- app/routes/node-route.js
+++ app/routes/node-route.js
@@ -1,4 +1,16 @@
+var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db){
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id' : new ObjectID(id)};
+        db.collection('notes').findOne(details, (err, item)=> {
+            if (err) {
+                res.send({'error': 'Error occured'});
+            } else {
+                res.send(item)
+            }
+        });
+    });
     app.post('/notes', (req, res) => {
         const note = { title: req.body.title, text: req.body.body};
         db.collection('notes').insert(note, (err, result)=>{
